d68dcea5a94f6fc75f0cb53761df86a4a2a3c322
League Student <league@iMac-14.attlocal.net>
2020-01-26 12:51:51 -0800

    i coded

--- src/_04_validation/Validation.java
+++ src/_04_validation/Validation.java
@@ -17,7 +17,22 @@ public class Validation {
 		System.out.println(randomNumber);
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
-int randomChoice = new Random ().nextInt();
+		for()
+		if(randomNumber==0) {
+			JOptionPane.showMessageDialog(null, "you are nice");
+		}
+			if(randomNumber==1) {
+				JOptionPane.showMessageDialog(null, "I like your shirt");
+			}
+				if(randomNumber==2) {
+					JOptionPane.showMessageDialog(null, "you are very fast");
+				}
+					if(randomNumber==3) {
+						JOptionPane.showMessageDialog(null, "you are very smart");
+					}
+						if(randomNumber==4) {
+							JOptionPane.showMessageDialog(null, "you have a lot of energy");
+						}
 		// 2. Repeat all the code above 10 times
 		
 		// 3. Find someone to test out your program. They will like it :)
